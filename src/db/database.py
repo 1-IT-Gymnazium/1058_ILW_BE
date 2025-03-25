e30e7c0e6b15cfb91351b5af5394f3686bf5d019
vojta mrkos <mrkosvojta@gmail.com>
2025-03-25 19:47:17 +0100

    changed db

--- src/db/database.py
+++ src/db/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from config import get_settings
 
+>>>>>>> HEAD
 DATABASE_URL = ""
+>>>>>>>
+
+DATABASE_URL = get_settings().neondb_string
+>>>>>>> 0611f6d (changed db)
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
